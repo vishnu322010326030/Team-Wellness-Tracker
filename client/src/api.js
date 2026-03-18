1a63fe3cc4391a0d545c03930f556ee0a0c2ae36
vishnu322010326030 <vpujariswarup@gmail.com>
2026-03-18 14:09:15 -0500

    add production API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
 });
 
-// Automatically attach token to every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
